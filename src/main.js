d03025b8501a012f96b8137af82a95f20a3e7f3c
Fabian Beck <fabian@fbeck.com>
2022-02-18 14:49:04 +0100

    release v0.1.0

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ Vue.config.productionTip = false
 Vue.prototype.$appName = "PURE suggest";
 Vue.prototype.$appNameHtml = '<span class="has-text-primary">PURE&nbsp;</span><span class="has-text-info">suggest</span>';
 Vue.prototype.$appSubtitle = "Citation-based literature search"
-Vue.prototype.$version = "0.1.0-beta";
+Vue.prototype.$version = "0.1.0";
 
 new Vue({
   render: h => h(App),
