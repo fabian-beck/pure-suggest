621bea19ebc64fae77b2b6105e92c613059b524c
Fabian Beck <fabian.beck@uni-bamberg.de>
2022-02-19 06:59:04 +0100

    user version number from package.json

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {version} from './../package.json';
 
 import Buefy from 'buefy'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -14,12 +15,12 @@ Vue.use(VueTippy);
 // eslint-disable-next-line vue/multi-word-component-names
 Vue.component("tippy", TippyComponent);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.prototype.$appName = "PURE suggest";
 Vue.prototype.$appNameHtml = '<span class="has-text-primary">PURE&nbsp;</span><span class="has-text-info">suggest</span>';
 Vue.prototype.$appSubtitle = "Citation-based literature search"
-Vue.prototype.$version = "0.1.0";
+Vue.prototype.$version = version;
 
 new Vue({
   render: h => h(App),
