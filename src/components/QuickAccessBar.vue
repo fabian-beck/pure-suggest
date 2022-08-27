502f995e8ea9ea25be4e1fe733240582782c70d3
Fabian Beck <fabian.beck@uni-bamberg.de>
2022-08-27 06:42:16 +0200

    Button labels for quick access bar

--- src/components/QuickAccessBar.vue
+++ src/components/QuickAccessBar.vue
@@ -5,18 +5,21 @@
       @click="scrollTo('selected')"
     >
       <b-icon icon="water-outline"></b-icon>
+      <div class="button-label">Selected</div>
     </button>
     <button
       class="button has-background-info has-text-white"
       @click="scrollTo('suggested')"
     >
       <b-icon icon="water-plus-outline"></b-icon>
+      <div class="button-label">Suggested</div>
     </button>
     <button
       class="button has-background-grey has-text-white"
       @click="scrollTo('network')"
     >
       <b-icon icon="chart-bubble"></b-icon>
+      <div class="button-label">Network</div>
     </button>
   </div>
 </template>
@@ -38,10 +41,17 @@ export default {
 button {
   margin: 0.5rem;
   box-shadow: 0.25rem 0.25rem 0.75rem grey;
+  width: 4.5rem;
+  height: 3.5rem;
+  display: inline;
+  padding: 0.25rem;
 
   & .icon {
-    height: 2rem !important;
-    width: 2rem !important;
+    margin: 0 !important;
+  }
+
+  & .button-label {
+    font-size: 0.7rem;
   }
 }
 </style>
