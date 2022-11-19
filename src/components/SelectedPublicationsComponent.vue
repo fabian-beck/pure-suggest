0beb2770fa8248bce60e93eca2c19c38da0bba57
Fabian Beck <fabian.beck@uni-bamberg.de>
2022-11-19 06:49:29 +0100

    Made boost keyword input a separate row

--- src/components/SelectedPublicationsComponent.vue
+++ src/components/SelectedPublicationsComponent.vue
@@ -12,184 +12,204 @@
         </div>
       </div>
       <div class="level-right" v-show="!sessionStore.isEmpty">
-        <div class="level-item">
-          <div class="field has-addons">
-            <p class="control has-icons-right">
-              <input
-                class="input boost has-background-warning-light"
-                type="text"
-                v-model="sessionStore.boostKeywordString"
-                placeholder="keyword(s)"
-                @keyup.enter="sessionStore.updateScores"
-                data-tippy-content="Boost by factors of 2 the score of publications that contain the following keyword(s) in their title.<br><br>Use commas to separate multiple keywords."
-                v-tippy
-              />
-              <span class="icon is-small is-right is-clickable">
-                <i class="delete" @click.stop="clearBoost"></i>
-              </span>
-            </p>
-            <p class="control">
-              <b-button
-                class="button has-background-warning"
-                type="submit"
-                icon-left="chevron-double-up"
-                v-on:click="sessionStore.updateScores"
-              >
-                <span><span class="key">B</span>oost</span>
-              </b-button>
-            </p>
-          </div>
-        </div>
+        <div class="level-item"></div>
       </div>
     </div>
-    <form v-on:submit.prevent="sessionStore.addPublicationByQuery" class="field has-addons">
-      <p class="control is-expanded">
-        <input
-          class="input add-publication"
-          type="text"
-          placeholder="DOI(s)/title or search"
-          v-model="sessionStore.addQuery"
-          data-tippy-content="One or more <b>DOIs</b> (separated by different characters or included in a different format such as BibTeX).<br/>Alternatively, <b>paper title</b> or a unique part of it (only works for one publication)"
-          v-tippy
-        />
-      </p>
-      <!-- copies of buttons necessary as just hiding text inside button leads to empty span tag inside buttons and layout issues -->
-      <p class="control">
-        <b-button
-          class="
-            button
-            level-right
-            has-background-primary-light
-            is-hidden-touch
-          "
-          :class="{ disabled: !sessionStore.addQuery }"
-          type="submit"
-          icon-left="plus-thick"
-          @click.stop="sessionStore.addPublicationByQuery"
-          ><span class="key">A</span>dd
-        </b-button>
-        <b-button
-          class="
-            button
-            level-right
-            has-background-primary-light
-            is-hidden-desktop
-          "
-          :class="{ disabled: !sessionStore.addQuery }"
-          type="submit"
-          icon-left="plus-thick"
-          @click.stop="sessionStore.addPublicationByQuery"
-        ></b-button>
-      </p>
-      <p class="control">
-        <b-button
-          class="
-            button
-            level-right
-            has-background-primary-light
-            ml-1
-            is-hidden-touch
-          "
-          type="submit"
-          icon-left="magnify"
-          @click.stop="interfaceStore.openSearch()"
-          ><span class="key">S</span>earch</b-button
-        >
-        <b-button
-          class="
-            button
-            level-right
-            has-background-primary-light
-            ml-1
-            is-hidden-desktop
-          "
-          type="submit"
-          icon-left="magnify"
-          @click.stop="interfaceStore.openSearch()"
-        ></b-button>
-      </p>
-    </form>
     <div>
-      <div
-        class="notification has-background-primary-light media p-2"
-        v-show="sessionStore.isUpdatable"
+      <form
+        v-on:submit.prevent="sessionStore.addPublicationByQuery"
+        class="field has-addons"
       >
-        <b-icon icon="tray-full" class="media-left ml-2 mt-2 is-hidden-mobile"></b-icon>
-        <div class="media-content has-text-centered mt-2">
-          <b>Queue:</b>
-          <span v-show="sessionStore.selectedQueue.length">
-            {{
-              sessionStore.selectedQueue.length > 1
-                ? `${sessionStore.selectedQueue.length} publications`
-                : "1 publication"
-            }}
-            to be added</span
-          ><span
-            v-show="
-              sessionStore.selectedQueue.length &&
-              sessionStore.excludedQueue.length
+        <p class="control is-expanded">
+          <input
+            class="input add-publication"
+            type="text"
+            placeholder="DOI(s)/title or search"
+            v-model="sessionStore.addQuery"
+            data-tippy-content="One or more <b>DOIs</b> (separated by different characters or included in a different format such as BibTeX).<br/>Alternatively, <b>paper title</b> or a unique part of it (only works for one publication)"
+            v-tippy
+          />
+        </p>
+        <!-- copies of buttons necessary as just hiding text inside button leads to empty span tag inside buttons and layout issues -->
+        <p class="control">
+          <b-button
+            class="
+              button
+              level-right
+              has-background-primary-light
+              is-hidden-touch
             "
-          >
-            and </span
-          ><span v-show="sessionStore.excludedQueue.length">
-            {{
-              sessionStore.excludedQueue.length > 1
-                ? `${sessionStore.excludedQueue.length} publications`
-                : "1 publication"
-            }}
-            to be excluded</span
-          >.
-        </div>
-        <div class="media-right level">
+            :class="{ disabled: !sessionStore.addQuery }"
+            type="submit"
+            icon-left="plus-thick"
+            @click.stop="sessionStore.addPublicationByQuery"
+            ><span class="key">A</span>dd
+          </b-button>
           <b-button
-            class="button ml-2 level-item"
-            icon-left="undo"
-            data-tippy-content="Clear queue and discard update."
-            v-tippy
-            @click.stop="sessionStore.clearQueues()"
-            ></b-button
+            class="
+              button
+              level-right
+              has-background-primary-light
+              is-hidden-desktop
+            "
+            :class="{ disabled: !sessionStore.addQuery }"
+            type="submit"
+            icon-left="plus-thick"
+            @click.stop="sessionStore.addPublicationByQuery"
+          ></b-button>
+        </p>
+        <p class="control">
+          <b-button
+            class="
+              button
+              level-right
+              has-background-primary-light
+              ml-1
+              is-hidden-touch
+            "
+            type="submit"
+            icon-left="magnify"
+            @click.stop="interfaceStore.openSearch()"
+            ><span class="key">S</span>earch</b-button
           >
           <b-button
-            class="button has-background-primary has-text-white ml-2 level-item"
-            icon-left="update"
-            data-tippy-content="Update suggested and excluded publications with queue and compute new suggestions."
-            v-tippy
-            @click="sessionStore.updateQueued"
-            ><span class="key">U</span>pdate</b-button
-          >
+            class="
+              button
+              level-right
+              has-background-primary-light
+              ml-1
+              is-hidden-desktop
+            "
+            type="submit"
+            icon-left="magnify"
+            @click.stop="interfaceStore.openSearch()"
+          ></b-button>
+        </p>
+      </form>
+      <div
+        class="notification has-background-warning-light p-2 pt-3 is-gapless"
+        v-show="!sessionStore.isEmpty"
+      >
+        <div class="field has-addons">
+          <p class="control has-icons-right is-expanded">
+            <input
+              class="input boost has-background-white"
+              type="text"
+              v-model="sessionStore.boostKeywordString"
+              placeholder="keyword(s)"
+              @keyup.enter="sessionStore.updateScores"
+              data-tippy-content="Boost by factors of 2 the score of publications that contain the following keyword(s) in their title.<br><br>Use commas to separate multiple keywords."
+              v-tippy
+            />
+            <span class="icon is-small is-right is-clickable">
+              <i class="delete" @click.stop="clearBoost"></i>
+            </span>
+          </p>
+          <p class="control">
+            <b-button
+              class="button has-background-warning"
+              type="submit"
+              icon-left="chevron-double-up"
+              v-on:click="sessionStore.updateScores"
+            >
+              <span><span class="key">B</span>oost</span>
+            </b-button>
+          </p>
         </div>
       </div>
-      <div
-        class="notification has-text-centered has-background-primary-light p-2"
-        v-show="sessionStore.isEmpty"
-      >
-        <p>
-          To start,
-          <b><b-icon icon="plus-thick" size="is-small"></b-icon> add</b>
-          publications through a <b>title</b> or
-          <a href="https://www.doi.org/"><b>DOI(s)</b></a
-          >, <br />
-          <b><b-icon icon="magnify" size="is-small"></b-icon> search</b> using
-          <b>keywords</b>, or:
-        </p>
-        <div class="level mt-2">
-          <div class="level-item">
-            <b-button
-              class="button has-background-primary-light"
-              icon-left="file-document"
-              @click.stop="sessionStore.loadExample()"
+      <div>
+        <div
+          class="notification has-background-primary-light media p-2"
+          v-show="sessionStore.isUpdatable"
+        >
+          <b-icon
+            icon="tray-full"
+            class="media-left ml-2 mt-2 is-hidden-mobile"
+          ></b-icon>
+          <div class="media-content has-text-centered mt-2">
+            <b>Queue:</b>
+            <span v-show="sessionStore.selectedQueue.length">
+              {{
+                sessionStore.selectedQueue.length > 1
+                  ? `${sessionStore.selectedQueue.length} publications`
+                  : "1 publication"
+              }}
+              to be added</span
+            ><span
+              v-show="
+                sessionStore.selectedQueue.length &&
+                sessionStore.excludedQueue.length
+              "
             >
-              Load example
-            </b-button>
+              and </span
+            ><span v-show="sessionStore.excludedQueue.length">
+              {{
+                sessionStore.excludedQueue.length > 1
+                  ? `${sessionStore.excludedQueue.length} publications`
+                  : "1 publication"
+              }}
+              to be excluded</span
+            >.
           </div>
-          <div class="level-item">
+          <div class="media-right level">
             <b-button
-              class="button has-background-primary-light"
-              icon-left="import"
-              @click="importSession"
+              class="button ml-2 level-item"
+              icon-left="undo"
+              data-tippy-content="Clear queue and discard update."
+              v-tippy
+              @click.stop="sessionStore.clearQueues()"
+            ></b-button>
+            <b-button
+              class="
+                button
+                has-background-primary has-text-white
+                ml-2
+                level-item
+              "
+              icon-left="update"
+              data-tippy-content="Update suggested and excluded publications with queue and compute new suggestions."
+              v-tippy
+              @click="sessionStore.updateQueued"
+              ><span class="key">U</span>pdate</b-button
             >
-              Import session from JSON
-            </b-button>
+          </div>
+        </div>
+        <div
+          class="
+            notification
+            has-text-centered has-background-primary-light
+            p-2
+          "
+          v-show="sessionStore.isEmpty"
+        >
+          <p>
+            To start,
+            <b><b-icon icon="plus-thick" size="is-small"></b-icon> add</b>
+            publications through a <b>title</b> or
+            <a href="https://www.doi.org/"><b>DOI(s)</b></a
+            >, <br />
+            <b><b-icon icon="magnify" size="is-small"></b-icon> search</b> using
+            <b>keywords</b>, or:
+          </p>
+          <div class="level mt-2">
+            <div class="level-item">
+              <b-button
+                class="button has-background-primary-light"
+                icon-left="file-document"
+                @click.stop="sessionStore.loadExample()"
+              >
+                Load example
+              </b-button>
+            </div>
+            <div class="level-item">
+              <b-button
+                class="button has-background-primary-light"
+                icon-left="import"
+                @click="importSession"
+              >
+                Import session from JSON
+              </b-button>
+            </div>
           </div>
         </div>
       </div>
@@ -248,7 +268,7 @@ export default {
 <style lang="scss" scoped>
 .box {
   display: grid;
-  grid-template-rows: max-content max-content max-content auto;
+  grid-template-rows: max-content max-content auto;
 
   & input.boost {
     text-transform: lowercase;
