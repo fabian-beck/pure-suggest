ab433c8f0e11eec7c76092e6016c171f176f7310
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-01-25 06:20:26 +0100

    Split DOIs more conservatively

--- src/PublicationSearch.js
+++ src/PublicationSearch.js
@@ -11,9 +11,9 @@ export default class PublicationSearch {
     async execute() {
         let dois = [];
         let results = [];
-        // splitting query by characters that must/should be encoded differently in DOIs or by typical prefixes
+        // splitting query by characters that must be encoded differently in DOIs or by typical prefixes
         // see: https://www.doi.org/doi_handbook/2_Numbering.html
-        this.query.split(/ |"|\{|\}|%|#|\?|<|>|\[|\]|\\|\||`|\+|doi:|doi.org\//).forEach((doi) => {
+        this.query.split(/ |"|%|#|\?|doi:|doi.org\//).forEach((doi) => {
             // cutting characters that might be included in DOI, but very unlikely at the end
             doi = _.trim(doi, ".,;");
             if (doi.indexOf("10.") === 0 && !dois.includes(doi)) {
