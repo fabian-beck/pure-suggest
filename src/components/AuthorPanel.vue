892205a7d07a1110bf9352b6ddea2fdf763f0568
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-07-27 17:13:29 +0200

    Author panel icons

--- src/components/AuthorPanel.vue
+++ src/components/AuthorPanel.vue
@@ -3,7 +3,8 @@
     <div class="card">
       <header class="card-header has-background-primary">
         <p class="card-header-title has-text-white">
-          <b-icon icon="account-group"></b-icon>&ensp;Authors of selected
+          <v-icon class="has-text-white">mdi-account-group</v-icon>
+          &ensp;Authors of selected
           publications
         </p>
       </header>
@@ -11,39 +12,23 @@
         <div class="content">
           <section>
             <ul>
-              <li
-                v-for="author in sessionStore.selectedPublicationsAuthors"
-                :key="author.id"
-                class="media"
-              >
+              <li v-for="author in sessionStore.selectedPublicationsAuthors" :key="author.id" class="media">
                 <div class="media-left">
-                  <b-icon
-                    icon="account"
-                    :size="authorIconSize(author.count)"
-                  ></b-icon>
+                  <v-icon :size="authorIconSize(author.count)">mdi-account</v-icon>
                 </div>
                 <div class="media-content">
                   <div class="content">
                     <div>
-                      <strong>{{ author.id }}</strong
-                      >&nbsp;<span v-if="author.orcid">
-                        <a :href="`https://orcid.org/${author.orcid}`"
-                          ><img
-                            alt="ORCID logo"
-                            src="https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png"
-                            width="14"
-                            height="14"
-                        /></a>
+                      <strong>{{ author.id }}</strong>&nbsp;<span v-if="author.orcid">
+                        <a :href="`https://orcid.org/${author.orcid}`"><img alt="ORCID logo"
+                            src="https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png" width="14"
+                            height="14" /></a>
                       </span>
                       <small v-if="author.alternativeNames.length > 1">
                         &ensp;also listed as
-                        <b-tag
-                          v-for="alternativeName in author.alternativeNames.filter(
-                            (name) => name !== author.id
-                          )"
-                          :key="alternativeName"
-                          class="tag alternative-name"
-                        >
+                        <b-tag v-for="alternativeName in author.alternativeNames.filter(
+                          (name) => name !== author.id
+                        )" :key="alternativeName" class="tag alternative-name">
                           {{ alternativeName }}
                         </b-tag>
                       </small>
@@ -51,35 +36,21 @@
                     <div>
                       <strong>{{ author.count }}</strong> selected publication{{
                         author.count > 1 ? "s" : ""
-                      }}<span v-if="author.yearMin != author.yearMax"
-                        >, {{ author.yearMin }} to {{ author.yearMax }} </span
-                      ><span v-else-if="author.yearMin"
-                        >, {{ author.yearMin }}</span
-                      >
+                      }}<span v-if="author.yearMin != author.yearMax">, {{ author.yearMin }} to {{ author.yearMax }}
+                      </span><span v-else-if="author.yearMin">, {{ author.yearMin }}</span>
                     </div>
-                    <div
-                      v-if="Object.keys(author.keywords).length > 0"
-                      class="is-size-7"
-                    >
-                      Related to<b-tag
-                        v-for="keyword in sessionStore.boostKeywords.filter(
-                          (keyword) => author.keywords[keyword]
-                        )"
-                        :key="keyword"
-                        :style="keywordStyle(author.keywords[keyword])"
-                        >{{ keyword }} ({{ author.keywords[keyword] }})
+                    <div v-if="Object.keys(author.keywords).length > 0" class="is-size-7">
+                      Related to<b-tag v-for="keyword in sessionStore.boostKeywords.filter(
+                        (keyword) => author.keywords[keyword]
+                      )" :key="keyword" :style="keywordStyle(author.keywords[keyword])">{{ keyword }} ({{
+  author.keywords[keyword] }})
                       </b-tag>
                     </div>
                     <div class="is-size-7">
                       Co-author of
-                      <b-tag
-                        v-for="coauthor in Object.keys(author.coauthors).sort(
-                          (a, b) => author.coauthors[b] - author.coauthors[a]
-                        )"
-                        :key="coauthor"
-                        class="tag coauthor"
-                        :style="coauthorStyle(author.coauthors[coauthor])"
-                      >
+                      <b-tag v-for="coauthor in Object.keys(author.coauthors).sort(
+                        (a, b) => author.coauthors[b] - author.coauthors[a]
+                      )" :key="coauthor" class="tag coauthor" :style="coauthorStyle(author.coauthors[coauthor])">
                         {{ coauthor }} ({{ author.coauthors[coauthor] }})
                       </b-tag>
                     </div>
@@ -108,28 +79,26 @@ export default {
   methods: {
     authorIconSize(count) {
       if (count > 5) {
-        return "is-large";
+        return "48";
       }
       if (count > 2) {
-        return "is-medium";
+        return "32";
       }
       if (count > 1) {
-        return null;
+        return "24";
       }
-      return "is-small";
+      return "18";
     },
     keywordStyle(count) {
       return {
-        backgroundColor: `hsla(48, 100%, 67%, ${
-          0.05 + Math.min(count / 20, 0.95)
-        })`,
+        backgroundColor: `hsla(48, 100%, 67%, ${0.05 + Math.min(count / 20, 0.95)
+          })`,
       };
     },
     coauthorStyle(count) {
       return {
-        backgroundColor: `hsla(0, 0%, 70%, ${
-          0.05 + Math.min(count / 20, 0.95)
-        })`,
+        backgroundColor: `hsla(0, 0%, 70%, ${0.05 + Math.min(count / 20, 0.95)
+          })`,
       };
     },
     cancel() {
@@ -155,6 +124,7 @@ export default {
 
   & .tag {
     margin: 0.25rem;
+
     &.keyword {
       text-decoration: underline;
       text-decoration-color: hsl(48, 100%, 67%);
