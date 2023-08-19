bb62f63609d702f50b68db8dd6cbb0324ec15f7b
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-08-19 14:00:47 +0200

    Header panel
    Not yet responsive

--- src/components/HeaderPanel.vue
+++ src/components/HeaderPanel.vue
@@ -1,111 +1,100 @@
 <template>
-  <div class="box p-0 m-0 is-radiusless">
-    <b-navbar
-      :fixed-top="interfaceStore.isMobile"
-      class="box p-0 m-0 is-radiusless"
-    >
-      <template #brand>
-        <b-navbar-item>
-          <b-icon
-            icon="water-plus-outline"
-            size="is-medium"
-            class="has-text-grey"
-          ></b-icon>
-          <h1 class="title ml-2">
-            <span v-html="$appNameHtml"></span>
-          </h1>
-        </b-navbar-item>
+  <v-app-bar color="white" dense :absolute="isMobile">
+    <v-icon class="mr-4" size="38">mdi-water-plus-outline</v-icon>
+    <v-toolbar-title>
+      <span v-html="$appNameHtml"></span>
+    </v-toolbar-title>
+    <v-menu v-if="!sessionStore.isEmpty" bottom right offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on" v-tippy title="Session" outlined class="ml-8">
+          <v-icon size="18" class="mr-2">mdi-text-box-multiple-outline</v-icon>
+          Session ({{ sessionStore.selectedPublicationsCount }} selected{{ sessionStore.excludedPublicationsCount
+            ? `; ${sessionStore.excludedPublicationsCount} excluded`
+            : "" }})
+          <v-icon class="ml-2">
+            mdi-menu-down
+          </v-icon>
+        </v-btn>
       </template>
-      <template #start>
-        <b-navbar-dropdown
-          :label="`Session (${sessionStore.selectedPublicationsCount} selected${
-            sessionStore.excludedPublicationsCount
-              ? `; ${sessionStore.excludedPublicationsCount} excluded`
-              : ''
-          })`"
-          v-show="!sessionStore.isEmpty"
-        >
-          <b-navbar-item @click="sessionStore.exportSession">
-            <b-icon icon="export"></b-icon>
-            <span class="ml-2">Export session as JSON</span>
-          </b-navbar-item>
-          <b-navbar-item @click="sessionStore.exportAllBibtex">
-            <b-icon icon="export"></b-icon>
-            <span class="ml-2">Export selected as BibTeX</span>
-          </b-navbar-item>
-          <b-navbar-item
-            @click="sessionStore.clearSession"
-            class="has-text-danger"
-          >
-            <b-icon icon="delete"></b-icon
-            ><span class="ml-2"><span class="key">C</span>lear session</span>
-          </b-navbar-item>
-        </b-navbar-dropdown>
+      <v-list>
+        <v-list-item @click="sessionStore.exportSession">
+          <v-list-item-icon>
+            <v-icon>mdi-export</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Export session as JSON</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="sessionStore.exportAllBibtex">
+          <v-list-item-icon>
+            <v-icon>mdi-export</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Export selected as BibTeX</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="sessionStore.clearSession" class="has-text-danger">
+          <v-list-item-icon>
+            <v-icon>mdi-delete</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Clear session</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <v-spacer></v-spacer>
+    <v-btn icon href="https://twitter.com/pure_suggest" v-tippy title="Follow us on Twitter">
+      <v-icon>mdi-twitter</v-icon>
+    </v-btn>
+    <v-btn icon href="https://medium.com/@pure_suggest" v-tippy title="Read more on Medium">
+      <v-icon>mdi-post</v-icon>
+    </v-btn>
+    <v-btn icon href="https://github.com/fabian-beck/pure-suggest" v-tippy title="Contribute on GitHub">
+      <v-icon>mdi-github</v-icon>
+    </v-btn>
+    <v-menu bottom left offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon v-bind="attrs" v-on="on" class="ml-4 mr-1">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
       </template>
-      <template #end>
-        <b-navbar-item tag="div" class="mr-6 ml-1">
-          <a href="https://twitter.com/pure_suggest" v-tippy title="Follow us on Twitter">
-            <b-icon icon="twitter"></b-icon>
-          </a>
-          <a href="https://medium.com/@pure_suggest" class="ml-5" v-tippy title="Read more on Medium">
-            <b-icon icon="post"></b-icon>
-          </a>
-          <a href="https://github.com/fabian-beck/pure-suggest" class="ml-5" v-tippy title="Contribute on GitHub">
-            <b-icon icon="github"></b-icon>
-          </a>
-        </b-navbar-item>
-        <b-navbar-dropdown
-          label="⋮"
-          icon-left="dots-vertical"
-          right
-          collapsible
-        >
-          <b-navbar-item @click="interfaceStore.openFeedback">
-            <b-icon icon="comment-quote-outline"></b-icon
-            ><span class="ml-2">Feedback</span>
-          </b-navbar-item>
-          <b-navbar-item
-            @click="interfaceStore.isKeyboardControlsModalDialogShown = true"
-            class="is-hidden-touch"
-          >
-            <b-icon icon="keyboard-outline"></b-icon
-            ><span class="ml-2">Keyboard controls</span>
-          </b-navbar-item>
-          <b-navbar-item @click="interfaceStore.isAboutModalDialogShown = true">
-            <b-icon icon="information-outline"></b-icon
-            ><span class="ml-2">About</span>
-          </b-navbar-item>
-          <b-navbar-item
-            @click="sessionStore.clearCache"
-            class="has-text-danger"
-          >
-            <b-icon icon="cached"></b-icon
-            ><span class="ml-2">Clear cache (and session)</span>
-          </b-navbar-item>
-        </b-navbar-dropdown>
-      </template>
-    </b-navbar>
-    <div class="columns" v-show="sessionStore.isEmpty">
-      <div class="column">
-        <div class="subtitle level-item mt-2">
-          {{ this.$appSubtitle }}
-        </div>
-      </div>
-      <div class="column is-two-thirds">
-        <div
-          class="notification has-text-centered p-2"
-          v-show="sessionStore.isEmpty"
-        >
-          <p>
-            Based on a set of selected publications,
-            <b class="has-text-info">suggest</b>ing related
-            <b class="has-text-primary">pu</b>blications connected by
-            <b class="has-text-primary">re</b>ferences.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+      <v-list>
+        <v-list-item @click="interfaceStore.openFeedback">
+          <v-list-item-icon>
+            <v-icon>mdi-comment-quote-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Feedback</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="interfaceStore.isKeyboardControlsModalDialogShown = true" class="is-hidden-touch">
+          <v-list-item-icon>
+            <v-icon>mdi-keyboard-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Keyboard controls</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="interfaceStore.isAboutModalDialogShown = true">
+          <v-list-item-icon>
+            <v-icon>mdi-information-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="sessionStore.clearCache" class="has-text-danger">
+          <v-list-item-icon>
+            <v-icon>mdi-cached</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Clear cache (and session)</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
 </template>
 
 <script>
@@ -126,18 +115,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.v-toolbar {
+  & ::v-deep {
+    & .v-toolbar__content {
+      padding: 0 0.5rem;
 
-.title {
-  font-size: $size-4;
-}
-
-.subtitle {
-  font-size: $size-5;
-}
-
-.columns {
-  & .column {
-    margin: $block-spacing;
+      & .v-toolbar__title {
+        font-size: 1.5rem;
+        font-weight: 600;
+      }
+    }
   }
 }
 </style>
