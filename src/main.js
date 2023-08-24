5b5e237251dfc0ac31de695ed8aa51b850c09ccf
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-08-24 14:45:53 +0200

    Removing old code

--- src/main.js
+++ src/main.js
@@ -42,15 +42,4 @@ app.use(VueTippy, {
 
 // Vue.config.productionTip = false;
 
-// Vue.prototype.$appName = "PURE suggest";
-// Vue.prototype.$appNameHtml = '<span class="has-text-primary">PURE&nbsp;</span><span class="has-text-info">suggest</span>';
-// Vue.prototype.$appSubtitle = "Citation-based literature search"
-// Vue.prototype.$version = packageInfo.version;
-
-// new Vue({
-//   pinia,
-//   vuetify,
-//   render: h => h(App),
-// }).$mount('#app')
-
 app.mount('#app');
