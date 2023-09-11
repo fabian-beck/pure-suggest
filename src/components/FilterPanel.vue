bc3c3f2419fd443e8677d7ff33129a891f06aa98
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-09-11 17:09:16 +0200

    Filter panel layout

--- src/components/FilterPanel.vue
+++ src/components/FilterPanel.vue
@@ -10,23 +10,25 @@
             <v-row>
                 <v-col cols="12" md="4">
                     <v-text-field label="Search" v-model="sessionStore.filter.string" placeholder="Text" density="compact"
-                        prepend-inner-icon="mdi-card-search" clearable hide-details />
+                        variant="underlined" prepend-inner-icon="mdi-card-search" clearable hide-details />
                 </v-col>
                 <v-col cols="12" md="4">
                     <v-row no-gutters>
                         <v-col cols="12" md="7">
                             <v-text-field label="Year from" v-model="sessionStore.filter.yearStart" placeholder="YYYY"
-                                density="compact" prepend-inner-icon="mdi-calendar" clearable :rules="yearRules" />
+                                density="compact" variant="underlined" prepend-inner-icon="mdi-calendar" clearable
+                                :rules="yearRules" validate-on="blur" />
                         </v-col>
                         <v-col cols="12" md="5">
                             <v-text-field label="to" v-model="sessionStore.filter.yearEnd" placeholder="YYYY"
-                                :rules="yearRules" density="compact" clearable />
+                                density="compact" variant="underlined" clearable :rules="yearRules" validate-on="blur" />
                         </v-col>
                     </v-row>
                 </v-col>
                 <v-col cols="12" md="4">
                     <v-select label="Tag" v-model="sessionStore.filter.tag" :items="Publication.TAGS" item-title="name"
-                        item-value="value" density="compact" prepend-inner-icon="mdi-tag" clearable hide-details />
+                        item-value="value" density="compact" variant="underlined" prepend-inner-icon="mdi-tag" clearable
+                        hide-details />
                 </v-col>
             </v-row>
         </form>
