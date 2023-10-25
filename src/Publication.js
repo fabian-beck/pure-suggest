e26b866d73b23ddf9a38df8bccb7d271167b2fed
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-10-25 05:56:00 +0200

    Fix: ORCID regex

--- src/Publication.js
+++ src/Publication.js
@@ -121,9 +121,9 @@ export default class Publication {
             } else if (authorArray.length > 2) {
                 this.authorShort = `${authorArray[0].split(', ')[0]} et al.`;
             }
-            this.author = data.author.replace(/(,\s+)(\d{4}-\d{4}-\d{4}-\d{3}[0-9X]{1})/g, "");
+            this.author = data.author.replace(/(,\s+)(\d{4}-\d{4}-\d{4}-\d{3}[0-9Xx]{1})/g, "");
             this.authorOrcid = data.author;
-            this.authorOrcidHtml = data.author.replace(/(,\s+)(\d{4}-\d{4}-\d{4}-\d{3}[0-9X]{1})/g, " <a href='https://orcid.org/$2'><img alt='ORCID logo' src='https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png' width='14' height='14' /></a>");
+            this.authorOrcidHtml = data.author.replace(/(,\s+)(\d{4}-\d{4}-\d{4}-\d{3}[0-9Xx]{1})/g, " <a href='https://orcid.org/$2'><img alt='ORCID logo' src='https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png' width='14' height='14' /></a>");
         }
         // container
         this.container = "";
