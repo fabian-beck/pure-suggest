f3d3706d1422caa5979a282bb55f497172f453ce
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-12-17 11:43:31 +0100

    Mark boost keywords

--- src/components/BoostKeywordsComponent.vue
+++ src/components/BoostKeywordsComponent.vue
@@ -1,9 +1,14 @@
 <template>
   <form @submit.prevent="sessionStore.updateScores"
     v-tippy="`Boost by factors of 2 the score of publications that contain the following keyword(s) in their title.`">
+    <div class="boost"  @click="isEditing = true" v-if="!isEditing">
+      <label>Boost keywords</label>
+      <div v-html="boostKeywordStringHtml"></div>
+    </div>
     <v-text-field class="boost" density="compact" v-model="sessionStore.boostKeywordString" label="Boost keywords"
       variant="solo" append-inner-icon="mdi-close" @click:append-inner="sessionStore.setBoostKeywordString('')"
-      hint="Use ',' to separate keywords, use '|' to discern alternatives/synonyms.">
+      hint="Use ',' to separate keywords, use '|' to discern alternatives/synonyms." v-if="isEditing"
+      @blur="isEditing = false">
       <template v-slot:append>
         <v-btn class="has-background-warning" @click="sessionStore.updateScores" height="47">
           <v-icon>mdi-chevron-double-up</v-icon>
@@ -24,6 +29,25 @@ export default {
     return { sessionStore };
   },
 
+  data() {
+    return {
+      isEditing: false,
+    };
+  },
+
+  computed: {
+    boostKeywordStringHtml() {
+      let html = this.sessionStore.boostKeywordString;
+      // wrap comma seperated words in span.word
+      html = html.replace(/([^,|]+)/g, "<span class='word'>$1</span>");
+      // wrap | in span.alt
+      html = html.replace(/\|/g, "<span class='alt'>|</span>");
+      // wrap , in span.comma
+      html = html.replace(/,/g, "<span class='comma'>,</span>");
+      return html;
+    },
+  },
+
 };
 </script>
 
@@ -41,5 +65,25 @@ export default {
     padding: 0 !important;
     min-width: 2rem;
   }
+}
+
+:deep(.word) {
+  text-decoration: underline;
+  text-decoration-color: hsl(48, 100%, 67%);
+  text-decoration-thickness: 0.2rem;
+}
+
+:deep(.alt) {
+  color: #aaa;
+  margin-left: 0.1rem;
+  margin-right: 0.1rem;
+  font-weight: bold;
+}
+
+:deep(.comma) {
+  color: #aaa;
+  margin-left: 0.2rem;
+  margin-right: 0.3rem;
+  font-weight: bold;
 }
 </style>
