3c4976a6614e6d5f8e5c0a2ce02f3303d029d76f
ba069016 <maximilian-emanuel.rittler@stud.uni-bamberg.de>
2024-10-03 16:28:09 +0200

    Introduces new  `myBibtexParser` function in `Util.js` to allow the extraction of DOIs from a BibTeX file. The function uses a regular expression to find all occurrences of DOIs and stores them in an array. The extracted DOIs are then returned as part of a session object.

--- src/Util.js
+++ src/Util.js
@@ -26,20 +26,52 @@ export function scrollToTargetAdjusted(element, offsetY) {
 
 // https://stackoverflow.com/questions/16801687/javascript-random-ordering-with-seed
 export function shuffle(array, seed) {
-
   function random(seed) {
     var x = Math.sin(seed++) * 10000;
     return x - Math.floor(x);
   }
 
-  var m = array.length, t, i;
+  var m = array.length,
+    t,
+    i;
   while (m) {
     i = Math.floor(random(seed) * m--);
     t = array[m];
     array[m] = array[i];
     array[i] = t;
-    ++seed
+    ++seed;
   }
   return array;
 }
 
+// This function is used to parse a BibTeX file and extract all DOIs from it and returns a "session"
+export function myBibtexParser(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.readAsText(file);
+
+    reader.onload = function (event) {
+      const content = event.target.result;
+
+      const dois = [];
+      const output = { selected: dois };
+
+      // Regex: Finds all occurences statring with DOI, any whitespace, =, any whitespace, match { or " , capture anything but } or " , and finally match } or }
+      const doiRegex = /doi\s*=\s*[{"]([^}"']+)[}"]/gi;
+
+      let match;
+      while ((match = doiRegex.exec(content)) !== null) {
+        const doi = match[1].trim();
+        dois.push(doi);
+      }
+
+      resolve(output);
+    };
+
+    // Error handling
+    reader.onerror = function () {
+      reject(new Error("Error reading the BibTeX file"));
+    };
+  });
+}
