4ff687b3613fe8d6a2523369652c49f8deba10cc
Fabian Beck <fabian.beck@uni-bamberg.de>
2025-09-11 15:08:35 +0200

    Add sorting by relevance to Crossref publication search
    Introduced a CROSSREF_SORT parameter in config and updated PublicationSearch to include sorting by relevance in Crossref API queries. This improves the quality of search results by returning the most relevant publications first.

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,19 +4,20 @@
 export const API_ENDPOINTS = {
   PUBLICATIONS: "https://pure-publications-cw3de4q5va-ew.a.run.app/",
   CROSSREF: "https://api.crossref.org/works",
-  GOOGLE_SCHOLAR: "https://scholar.google.de/scholar"
+  GOOGLE_SCHOLAR: "https://scholar.google.de/scholar",
 };
 
 export const API_PARAMS = {
   NO_CACHE_PARAM: "&noCache=true",
   CROSSREF_EMAIL: "fabian.beck@uni-bamberg.de",
-  CROSSREF_FILTER: "has-references:true"
+  CROSSREF_FILTER: "has-references:true",
+  CROSSREF_SORT: "relevance",
 };
 
 // Pagination Configuration
 export const PAGINATION = {
   LOAD_MORE_INCREMENT: 100,
-  INITIAL_SUGGESTIONS_COUNT: 100
+  INITIAL_SUGGESTIONS_COUNT: 100,
 };
 
 // Scoring Configuration
@@ -24,7 +25,7 @@ export const SCORING = {
   DEFAULT_BOOST_FACTOR: 1,
   BOOST_MULTIPLIER: 2,
   FIRST_AUTHOR_BOOST: 2,
-  NEW_PUBLICATION_BOOST: 2
+  NEW_PUBLICATION_BOOST: 2,
 };
 
 // Time Configuration
--- src/core/PublicationSearch.js
+++ src/core/PublicationSearch.js
@@ -3,46 +3,54 @@ import Publication from "./Publication.js";
 import { API_ENDPOINTS, API_PARAMS } from "../constants/config.js";
 
 export default class PublicationSearch {
+  constructor(query) {
+    this.query = query;
+  }
 
-    constructor(query) {
-        this.query = query;
+  async execute() {
+    let dois = [];
+    let results = [];
+    // removing whitespace (e.g., through line breaks) in DOIs
+    this.query = this.query.replace(
+      /(10\.\d+\/)\s?(\S{0,12})\s([^[])/g,
+      "$1$2$3"
+    );
+    // splitting query by characters that must (or partly: should) be encoded differently in DOIs or by typical prefixes
+    // see: https://www.doi.org/doi_handbook/2_Numbering.html
+    // "\{|\}" necessary to read DOIs from BibTeX
+    this.query.split(/ |"|%|#|\?|\{|\}|doi:|doi.org\//).forEach((doi) => {
+      // cutting characters that might be included in DOI, but very unlikely at the end
+      doi = doi
+        .trim()
+        .replace(/^[.,;]+|[.,;]+$/g, "")
+        .replace("\\_", "_");
+      if (doi.indexOf("10.") === 0 && !dois.includes(doi)) {
+        dois.push(doi);
+        const publication = new Publication(doi);
+        publication.fetchData();
+        results.push(publication);
+      }
+    });
+    if (dois.length) {
+      console.log(
+        `Identified ${results.length} DOI(s) in input; do not perform search.`
+      );
+      return { results: results, type: "doi" };
     }
-
-    async execute() {
-        let dois = [];
-        let results = [];
-        // removing whitespace (e.g., through line breaks) in DOIs
-        this.query = this.query.replace(/(10\.\d+\/)\s?(\S{0,12})\s([^[])/g, "$1$2$3");
-        // splitting query by characters that must (or partly: should) be encoded differently in DOIs or by typical prefixes
-        // see: https://www.doi.org/doi_handbook/2_Numbering.html
-        // "\{|\}" necessary to read DOIs from BibTeX
-        this.query.split(/ |"|%|#|\?|\{|\}|doi:|doi.org\//).forEach((doi) => {
-            // cutting characters that might be included in DOI, but very unlikely at the end
-            doi = doi.trim().replace(/^[.,;]+|[.,;]+$/g, '').replace("\\_", "_");
-            if (doi.indexOf("10.") === 0 && !dois.includes(doi)) {
-                dois.push(doi);
-                const publication = new Publication(doi);
-                publication.fetchData();
-                results.push(publication);
-            }
-        });
-        if (dois.length) {
-            console.log(`Identified ${results.length} DOI(s) in input; do not perform search.`)
-            return {results: results, type: "doi"};
-        }
-        const simplifiedQuery = this.query.replace(/\W+/g, "+").toLowerCase();
-        console.log(`Searching for publications matching '${this.query}'.`)
-        await cachedFetch(
-            `${API_ENDPOINTS.CROSSREF}?query=${simplifiedQuery}&mailto=${API_PARAMS.CROSSREF_EMAIL}&filter=${API_PARAMS.CROSSREF_FILTER}`,
-            (data) => {
-                data.message.items.filter(item => item.title).forEach((item) => {
-                    const publication = new Publication(item.DOI);
-                    publication.fetchData();
-                    results.push(publication);
-                });
-            }
-        );
-        return {results: results, type: "search"};
-    }
-
+    const simplifiedQuery = this.query.replace(/\W+/g, "+").toLowerCase();
+    console.log(`Searching for publications matching '${this.query}'.`);
+    await cachedFetch(
+      `${API_ENDPOINTS.CROSSREF}?query=${simplifiedQuery}&mailto=${API_PARAMS.CROSSREF_EMAIL}&filter=${API_PARAMS.CROSSREF_FILTER}&sort=${API_PARAMS.CROSSREF_SORT}&order=desc`,
+      (data) => {
+        data.message.items
+          .filter((item) => item.title)
+          .forEach((item) => {
+            const publication = new Publication(item.DOI);
+            publication.fetchData();
+            results.push(publication);
+          });
+      }
+    );
+    return { results: results, type: "search" };
+  }
 }
